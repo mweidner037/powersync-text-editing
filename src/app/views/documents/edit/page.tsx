6d7ca0f6eff74d9da88adac733dc86611469793f
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2025-08-23 14:38:57 -0500

    separate editor component

--- src/app/views/documents/edit/page.tsx
+++ src/app/views/documents/edit/page.tsx
@@ -1,263 +1,14 @@
-import { usePowerSync, useQuery } from '@powersync/react';
-import { Box, Button, CircularProgress, Typography, styled } from '@mui/material';
-import Fab from '@mui/material/Fab';
-import { MutableRefObject, Suspense, useEffect, useRef } from 'react';
+import { useQuery } from '@powersync/react';
+import { Box, CircularProgress, Typography } from '@mui/material';
+import { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSupabase } from '@/components/providers/SystemProvider';
-import { DOCUMENTS_TABLE, SHARED_CURSORS_TABLE, TEXT_UPDATES_TABLE } from '@/library/powersync/AppSchema';
+import { DOCUMENTS_TABLE } from '@/library/powersync/AppSchema';
 import { NavigationPage } from '@/components/navigation/NavigationPage';
-import { useEditor, EditorContent, Editor } from '@tiptap/react';
 import './styles.css';
-import { useReducedTable } from '@/library/powersync/use_reduced_table';
-import { buildTiptapExtensions } from '@/library/tiptap/extensions';
-import {
-  CollabTiptapStep,
-  collabTiptapStepReducer,
-  ElementIdGenerator,
-  updateToSteps
-} from '@/library/tiptap/step_converter';
-import { IdList } from 'articulated';
-import { IdSelection, selectionFromIds, selectionToIds } from '@/library/tiptap/selection';
-import { TextSelection } from '@tiptap/pm/state';
-import MenuBar from '../../../../components/editor/MenuBar';
-import { getIdListState, setIdListState } from '@/library/tiptap/plugins/id-list-state';
-import { v4 as uuidv4 } from 'uuid';
-import { randomName, randomColor } from '@/library/utils';
-import { SharedCursor } from '@/library/tiptap/plugins/shared-cursors';
-import _ from 'lodash';
 import { SetPowerSyncParams } from '@/components/widgets/SetPowerSyncParams';
 import { GuardBySync } from '@/components/widgets/GuardBySync';
-
-interface UserData {
-  name: string;
-  color: string;
-}
-
-const DocumentEditSection = () => {
-  // PowerSync queries
-
-  const powerSync = usePowerSync();
-  const supabase = useSupabase();
-  const { id: docID } = useParams();
-
-  const {
-    data: [documentRecord]
-  } = useQuery<{ name: string }>(`SELECT name FROM ${DOCUMENTS_TABLE} WHERE id = ?`, [docID]);
-
-  // Our shared cursor
-
-  const clientIdRef = useRef('');
-  const userDataRef = useRef<UserData>({ name: '', color: '' });
-  if (!clientIdRef.current) {
-    // This needs to unique to the editor instance - can't be userId.
-    const clientId = uuidv4();
-    clientIdRef.current = clientId;
-    userDataRef.current = {
-      // TODO: Get name from account?
-      name: randomName(),
-      color: randomColor()
-    };
-  }
-
-  useEffect(() => {
-    void powerSync.execute(
-      `INSERT INTO ${SHARED_CURSORS_TABLE} (id, doc_id, expires_at, user_data, selection)
-    VALUES (?, ?, (datetime('now', '+30 seconds')), ?, ?)`,
-      [clientIdRef.current, docID, JSON.stringify(userDataRef.current), null]
-    );
-
-    return () => {
-      // Best-effort delete. If this fails, the row will expire shortly.
-      void powerSync.execute(`DELETE FROM ${SHARED_CURSORS_TABLE} WHERE id = ?`, [clientIdRef.current]);
-    };
-  }, []);
-
-  // PowerSync mutations
-
-  const pendingUpdateCounterRef = useRef(0);
-
-  const doUpdate = async (update: CollabTiptapStep[]) => {
-    const userID = supabase?.currentSession?.user.id;
-    if (!userID) {
-      throw new Error(`Could not get user ID.`);
-    }
-
-    pendingUpdateCounterRef.current++;
-    try {
-      await powerSync.execute(
-        `INSERT INTO
-                ${TEXT_UPDATES_TABLE}
-                    (id, created_at, created_by, "update", doc_id)
-                VALUES
-                    (uuid(), datetime(), ?, ?, ?)`,
-        [userID, JSON.stringify(update), docID!]
-      );
-    } finally {
-      pendingUpdateCounterRef.current--;
-    }
-  };
-  const updatedSharedCursor = _.throttle(async (selection: IdSelection) => {
-    await powerSync.execute(
-      `UPDATE ${SHARED_CURSORS_TABLE} SET expires_at = (datetime('now', '+30 seconds')), selection = ? WHERE id = ?`,
-      [JSON.stringify(selection), clientIdRef.current]
-    );
-  }, 500);
-  const clear = async () => {
-    await powerSync.execute(`DELETE FROM ${TEXT_UPDATES_TABLE} WHERE doc_id = ?`, [docID!]);
-    await powerSync.execute(`DELETE FROM ${SHARED_CURSORS_TABLE} WHERE doc_id = ?`, [docID!]);
-  };
-
-  // Tiptap setup
-
-  const idGenRef = useRef<ElementIdGenerator>(new ElementIdGenerator());
-  const editor = useEditor({
-    extensions: buildTiptapExtensions(clientIdRef.current),
-    // We update the editor's state each render with a tr, so turn this off
-    // to prevent an infinite rerender loop.
-    shouldRerenderOnTransaction: false,
-    onUpdate({ transaction, editor }) {
-      const [steps, newIdList] = updateToSteps(transaction, getIdListState(editor.state).idList, idGenRef.current);
-      // It would be cleaner to add the new IdList to transaction and then dispatch it,
-      // like when using ProseMirror's dispatchTransaction prop.
-      // That way the state is updated before any local plugins see this transaction.
-      // With Tiptap, we instead need to update it afterwards and trust plugins to not
-      // read the state on local updates.
-      editor.commands.setIdListState(newIdList);
-
-      if (steps.length > 0) void doUpdate(steps);
-    },
-    onSelectionUpdate({ transaction, editor }) {
-      if (transaction.getMeta('ourRemoteUpdate')) return;
-
-      const { isValid, idList } = getIdListState(editor.state);
-      if (!isValid) return;
-      const idSelection = selectionToIds(editor.state.selection, idList);
-
-      void updatedSharedCursor(idSelection);
-
-      // TODO: also null/set on focus in/out, like y-cursor.
-      // TODO: Hearbeat
-    }
-  });
-
-  // Render
-
-  if (!documentRecord) {
-    return (
-      <Box>
-        <Typography>No matching document found, please navigate back...</Typography>
-      </Box>
-    );
-  }
-
-  return (
-    <NavigationPage title={`Document: ${documentRecord.name}`}>
-      <Box>
-        <MenuBar editor={editor} />
-        <EditorContent editor={editor} />
-        {editor ? (
-          <>
-            <EditorController
-              key={docID + '-editor'}
-              docID={docID!}
-              editor={editor}
-              pendingUpdateCounterRef={pendingUpdateCounterRef}
-            />
-            <SharedCursorQuery key={docID + '-cursors'} docID={docID!} editor={editor} />
-          </>
-        ) : null}
-        <Button onClick={clear}>Clear</Button>
-      </Box>
-    </NavigationPage>
-  );
-};
-
-function EditorController({
-  docID,
-  editor,
-  pendingUpdateCounterRef
-}: {
-  docID: string;
-  editor: Editor;
-  pendingUpdateCounterRef: MutableRefObject<number>;
-}) {
-  // On each render, set the editor's state to that indicated by TEXT_UPDATES_TABLE,
-  // and update idListRef to match.
-  // Except, preserve the selection in a collaboration-aware way using IdList.
-
-  const startingIdList = getIdListState(editor.state).idList;
-
-  // - Reset the state, since we (re-)apply all updates below.
-  const tr = editor.state.tr;
-  tr.delete(0, tr.doc.content.size);
-  const initialSize = tr.doc.content.size;
-  const initialIdList = IdList.new().insertAfter(null, { bunchId: 'init', counter: 0 }, initialSize);
-
-  // - Apply all updates to tr and idListRef.
-  const { data: reducedResult, isFetching } = useReducedTable(
-    TEXT_UPDATES_TABLE,
-    docID,
-    { tr, idList: initialIdList },
-    collabTiptapStepReducer
-  );
-
-  if (isFetching || pendingUpdateCounterRef.current > 0) {
-    // After performing a local update, we need to wait for the reduced state to be at least
-    // as up-to-date before touching the editor.
-    // Otherwise, the editor's state goes backwards, potentially causing jitter
-    // and also confusing our restore-selection code.
-    // Empirically, the reduced state is updated once:
-    // - All of the pending powerSync.execute promises have resolved, and
-    // - isFetching is false.
-    return null;
-  }
-
-  setIdListState(tr, reducedResult.idList);
-
-  // - Restore the starting selection in a collaboration-aware way.
-  // We do this by converting the initial selection to ElementIds and back.
-  const startingSelection = editor.state.selection;
-  const idSelection = selectionToIds(startingSelection, startingIdList);
-  try {
-    tr.setSelection(selectionFromIds(idSelection, tr.doc, reducedResult.idList));
-  } catch (error) {
-    // This can happen naturally if the state goes backwards somehow. Clear the selection and don't crash.
-    tr.setSelection(TextSelection.create(tr.doc, 0));
-    console.error('Error restoring selection', error);
-  }
-
-  // - Update the editor.
-  tr.setMeta('ourRemoteUpdate', true);
-  editor.view.updateState(editor.state.apply(tr));
-
-  // Not a real component, just a wrapper for hooks.
-  return null;
-}
-
-function SharedCursorQuery({ docID, editor }: { docID: string; editor: Editor }) {
-  // TODO: Need to rerun this on a timer, not just when data changes (since now() also changes).
-  // Could do that always to debounce as well.
-  const { data: cursorRows } = useQuery<{ id: string; user_data: string; selection: string | null }>(
-    `
-    SELECT id, user_data, selection FROM ${SHARED_CURSORS_TABLE}
-    WHERE doc_id=?
-    AND datetime('now') < expires_at`,
-    [docID]
-  );
-  // TODO: clock sync issues. Currently we're trusting all clients to be in sync.
-  // Setting a server expires_at would need a trigger and still won't be in sync with clients.
-
-  const cursors = cursorRows.map(
-    ({ id, user_data, selection }): SharedCursor => ({
-      clientId: id,
-      selection: selection ? JSON.parse(selection) : null,
-      user: JSON.parse(user_data)
-    })
-  );
-  editor.commands.setSharedCursors(cursors);
-
-  return null;
-}
+import { PowerSyncTextEditor } from '@/components/editor/PowerSyncTextEditor';
 
 export default function DocumentEditPage() {
   const { id: docID } = useParams();
@@ -275,7 +26,7 @@ export default function DocumentEditPage() {
             (including when we are logged in anonymously). */}
         <SetPowerSyncParams connector={supabase} params={{ current_doc_id: docID }}>
           <GuardBySync>
-            <DocumentEditSection />
+            <DocumentEditSection docID={docID!} />
           </GuardBySync>
         </SetPowerSyncParams>
       </Suspense>
@@ -283,10 +34,22 @@ export default function DocumentEditPage() {
   );
 }
 
-namespace S {
-  export const FloatingActionButton = styled(Fab)`
-    position: absolute;
-    bottom: 20px;
-    right: 20px;
-  `;
-}
+const DocumentEditSection = ({ docID }: { docID: string }) => {
+  const {
+    data: [documentRecord]
+  } = useQuery<{ name: string }>(`SELECT name FROM ${DOCUMENTS_TABLE} WHERE id = ?`, [docID]);
+
+  if (!documentRecord) {
+    return (
+      <Box>
+        <Typography>No matching document found, please navigate back...</Typography>
+      </Box>
+    );
+  }
+
+  return (
+    <NavigationPage title={`Document: ${documentRecord.name}`}>
+      <PowerSyncTextEditor docID={docID} />
+    </NavigationPage>
+  );
+};
---
+++ src/components/editor/PowerSyncTextEditor.tsx
@@ -0,0 +1,237 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+import { SHARED_CURSORS_TABLE, TEXT_UPDATES_TABLE } from '@/library/powersync/AppSchema';
+import { Editor, EditorContent, useEditor } from '@tiptap/react';
+import { buildTiptapExtensions } from '@/library/tiptap/extensions';
+import {
+  CollabTiptapStep,
+  collabTiptapStepReducer,
+  ElementIdGenerator,
+  updateToSteps
+} from '@/library/tiptap/step_converter';
+import { IdSelection, selectionFromIds, selectionToIds } from '@/library/tiptap/selection';
+import { getIdListState, setIdListState } from '@/library/tiptap/plugins/id-list-state';
+import { v4 as uuidv4 } from 'uuid';
+import { randomName, randomColor } from '@/library/utils';
+import _ from 'lodash';
+import { usePowerSync, useQuery } from '@powersync/react';
+import { useSupabase } from '../providers/SystemProvider';
+import { Box, Button } from '@mui/material';
+import MenuBar from './MenuBar';
+import { useReducedTable } from '@/library/powersync/use_reduced_table';
+import { SharedCursor } from '@/library/tiptap/plugins/shared-cursors';
+import { TextSelection } from '@tiptap/pm/state';
+import { IdList } from 'articulated';
+
+interface UserData {
+  name: string;
+  color: string;
+}
+
+export interface PowerSyncTextEditorProps {
+  docID: string;
+}
+
+export const PowerSyncTextEditor = ({ docID }: PowerSyncTextEditorProps) => {
+  const powerSync = usePowerSync();
+  const supabase = useSupabase();
+
+  // Our shared cursor
+
+  const clientIdRef = useRef('');
+  const userDataRef = useRef<UserData>({ name: '', color: '' });
+  if (!clientIdRef.current) {
+    // This needs to unique to the editor instance - can't be userId.
+    const clientId = uuidv4();
+    clientIdRef.current = clientId;
+    userDataRef.current = {
+      // TODO: Get name from account?
+      name: randomName(),
+      color: randomColor()
+    };
+  }
+
+  useEffect(() => {
+    void powerSync.execute(
+      `INSERT INTO ${SHARED_CURSORS_TABLE} (id, doc_id, expires_at, user_data, selection)
+    VALUES (?, ?, (datetime('now', '+30 seconds')), ?, ?)`,
+      [clientIdRef.current, docID, JSON.stringify(userDataRef.current), null]
+    );
+
+    return () => {
+      // Best-effort delete. If this fails, the row will expire shortly.
+      void powerSync.execute(`DELETE FROM ${SHARED_CURSORS_TABLE} WHERE id = ?`, [clientIdRef.current]);
+    };
+  }, []);
+
+  // PowerSync mutations
+
+  const pendingUpdateCounterRef = useRef(0);
+
+  const doUpdate = async (update: CollabTiptapStep[]) => {
+    const userID = supabase?.currentSession?.user.id;
+    if (!userID) {
+      throw new Error(`Could not get user ID.`);
+    }
+
+    pendingUpdateCounterRef.current++;
+    try {
+      await powerSync.execute(
+        `INSERT INTO
+                ${TEXT_UPDATES_TABLE}
+                    (id, created_at, created_by, "update", doc_id)
+                VALUES
+                    (uuid(), datetime(), ?, ?, ?)`,
+        [userID, JSON.stringify(update), docID!]
+      );
+    } finally {
+      pendingUpdateCounterRef.current--;
+    }
+  };
+
+  const clear = async () => {
+    await powerSync.execute(`DELETE FROM ${TEXT_UPDATES_TABLE} WHERE doc_id = ?`, [docID!]);
+    await powerSync.execute(`DELETE FROM ${SHARED_CURSORS_TABLE} WHERE doc_id = ?`, [docID!]);
+  };
+
+  const updatedSharedCursor = _.throttle(async (selection: IdSelection) => {
+    await powerSync.execute(
+      `UPDATE ${SHARED_CURSORS_TABLE} SET expires_at = (datetime('now', '+30 seconds')), selection = ? WHERE id = ?`,
+      [JSON.stringify(selection), clientIdRef.current]
+    );
+  }, 500);
+
+  // Tiptap setup
+
+  const idGenRef = useRef<ElementIdGenerator>(new ElementIdGenerator());
+  const editor = useEditor({
+    extensions: buildTiptapExtensions(clientIdRef.current),
+    // We update the editor's state each render with a tr, so turn this off
+    // to prevent an infinite rerender loop.
+    shouldRerenderOnTransaction: false,
+    onUpdate({ transaction, editor }) {
+      const [steps, newIdList] = updateToSteps(transaction, getIdListState(editor.state).idList, idGenRef.current);
+      // It would be cleaner to add the new IdList to transaction and then dispatch it,
+      // like when using ProseMirror's dispatchTransaction prop.
+      // That way the state is updated before any local plugins see this transaction.
+      // With Tiptap, we instead need to update it afterwards and trust plugins to not
+      // read the state on local updates.
+      editor.commands.setIdListState(newIdList);
+
+      if (steps.length > 0) void doUpdate(steps);
+    },
+    onSelectionUpdate({ transaction, editor }) {
+      if (transaction.getMeta('ourRemoteUpdate')) return;
+
+      const { isValid, idList } = getIdListState(editor.state);
+      if (!isValid) return;
+      const idSelection = selectionToIds(editor.state.selection, idList);
+
+      void updatedSharedCursor(idSelection);
+
+      // TODO: also null/set on focus in/out, like y-cursor.
+      // TODO: Hearbeat
+    }
+  });
+
+  return (
+    <Box>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+      <EditorController
+        key={docID + '-editor'}
+        docID={docID!}
+        editor={editor}
+        pendingUpdateCounterRef={pendingUpdateCounterRef}
+      />
+      <SharedCursorQuery key={docID + '-cursors'} docID={docID!} editor={editor} />
+      <Button onClick={clear}>Clear</Button>
+    </Box>
+  );
+};
+
+function EditorController({
+  docID,
+  editor,
+  pendingUpdateCounterRef
+}: {
+  docID: string;
+  editor: Editor;
+  pendingUpdateCounterRef: MutableRefObject<number>;
+}) {
+  // On each render, set the editor's state to that indicated by TEXT_UPDATES_TABLE,
+  // and update idListRef to match.
+  // Except, preserve the selection in a collaboration-aware way using IdList.
+
+  const startingIdList = getIdListState(editor.state).idList;
+
+  // - Reset the state, since we (re-)apply all updates below.
+  const tr = editor.state.tr;
+  tr.delete(0, tr.doc.content.size);
+  const initialSize = tr.doc.content.size;
+  const initialIdList = IdList.new().insertAfter(null, { bunchId: 'init', counter: 0 }, initialSize);
+
+  // - Apply all updates to tr and idListRef.
+  const { data: reducedResult, isFetching } = useReducedTable(
+    TEXT_UPDATES_TABLE,
+    docID,
+    { tr, idList: initialIdList },
+    collabTiptapStepReducer
+  );
+
+  if (isFetching || pendingUpdateCounterRef.current > 0) {
+    // After performing a local update, we need to wait for the reduced state to be at least
+    // as up-to-date before touching the editor.
+    // Otherwise, the editor's state goes backwards, potentially causing jitter
+    // and also confusing our restore-selection code.
+    // Empirically, the reduced state is updated once:
+    // - All of the pending powerSync.execute promises have resolved, and
+    // - isFetching is false.
+    return null;
+  }
+
+  setIdListState(tr, reducedResult.idList);
+
+  // - Restore the starting selection in a collaboration-aware way.
+  // We do this by converting the initial selection to ElementIds and back.
+  const startingSelection = editor.state.selection;
+  const idSelection = selectionToIds(startingSelection, startingIdList);
+  try {
+    tr.setSelection(selectionFromIds(idSelection, tr.doc, reducedResult.idList));
+  } catch (error) {
+    // This can happen naturally if the state goes backwards somehow. Clear the selection and don't crash.
+    tr.setSelection(TextSelection.create(tr.doc, 0));
+    console.error('Error restoring selection', error);
+  }
+
+  // - Update the editor.
+  tr.setMeta('ourRemoteUpdate', true);
+  editor.view.updateState(editor.state.apply(tr));
+
+  // Not a real component, just a wrapper for hooks.
+  return null;
+}
+
+function SharedCursorQuery({ docID, editor }: { docID: string; editor: Editor }) {
+  // TODO: Need to rerun this on a timer, not just when data changes (since now() also changes).
+  // Could do that always to debounce as well.
+  const { data: cursorRows } = useQuery<{ id: string; user_data: string; selection: string | null }>(
+    `
+    SELECT id, user_data, selection FROM ${SHARED_CURSORS_TABLE}
+    WHERE doc_id=?
+    AND datetime('now') < expires_at`,
+    [docID]
+  );
+  // TODO: clock sync issues. Currently we're trusting all clients to be in sync.
+  // Setting a server expires_at would need a trigger and still won't be in sync with clients.
+
+  const cursors = cursorRows.map(
+    ({ id, user_data, selection }): SharedCursor => ({
+      clientId: id,
+      selection: selection ? JSON.parse(selection) : null,
+      user: JSON.parse(user_data)
+    })
+  );
+  editor.commands.setSharedCursors(cursors);
+
+  return null;
+}
